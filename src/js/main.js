319d44f9b51aa5c1ce390096a83335c24ab7f2fd
Nick <tupacls96@gmail.com>
2022-10-17 13:58:42 +0300

    add humberger, fix a few intarfac bugs

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+        
+        const humburger  = document.querySelector('.hamburger-box'),
+                menu = document.querySelector('.hamburger-menu');
+
+       
+        function hideHumburger () {
+                menu.classList.add('hide');
+                humburger.addEventListener('click', (event) => {
+                        const e = event.target;
+                                if(menu.classList.contains('hide')) {
+                                        menu.classList.add('show', 'fade');
+                                        menu.classList.remove('hide');
+                                } else {
+                                        menu.classList.remove('show', 'fadegit ');
+                                        menu.classList.add('hide');
+
+                        }
+                });
+        }
+        hideHumburger();
+        });
